15f1a760ef1699fcad784c8fff43b9c257fc4ca6
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-08 14:20:34 -0800

    Add product update and delete functionality for admin.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   before_filter :authorize, except: [:index, :show]
 
   def index
+    @user = current_user
     @products = Product.all
     @order_item = current_order.order_items.new
   end
@@ -20,6 +21,29 @@ class ProductsController < ApplicationController
       redirect_to '/'
     else
       render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "#{@product.name} has been updated!"
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_path }
+      format.js
     end
   end
 
