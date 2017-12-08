b8424929ae333ef42a5ed70de33706e3b13f379a
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-08 12:05:40 -0800

    Add flash messages for signing up, signing in, and signing out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:notice] = "Welcome! Your account has been created!"
       redirect_to '/'
     else
       redirect_to '/signup'
